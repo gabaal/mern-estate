4cddf597e61265ebca5a54b659e746b501010f50
Gerald <geraldbaalham@live.co.uk>
2024-04-11 20:35:17 +0100

    Completed create listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
   signOutUserSuccess,
   signOutUserFailure,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 // Firebase storge rule
 // allow read;
@@ -189,6 +190,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
